b09493e5eeb518ff2409c00287c5daa25cceb5cd
Trailblazer-Yoo <yousun9591@naver.com>
2022-04-11 22:23:09 +0900

    final

--- promise.py
+++ promise.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from collections import Counter
-
+from PIL import Image
 import preprocessing
 import execute
 
@@ -173,7 +173,8 @@ elif chapter == '클러스터링':
         options=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
         value=4)
         st.markdown('> 저희의 분석 결과 최적의 군집 개수는 4 입니다.')
-        st.pyplot(km.UMAP_show(number))
+        with Image.open(f'{root_path}umap/umap_{number}.png') as f:
+            st.image(f)
         
 
 # 추천
